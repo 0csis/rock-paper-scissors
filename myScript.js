bd8b8c9b135ad40e85c5b8289e0ec5e173d49ad1
0csis <121513547+0csis@users.noreply.github.com>
2023-02-14 15:59:15 -0300

    add playRound function

--- myScript.js
+++ myScript.js
@@ -14,3 +14,37 @@ function getComputerChoice() {
   }
 }
 
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1,);
+
+  if (playerSelection == 'Rock') {
+    if (computerSelection == 'Scissors') {
+      return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else if (computerSelection == 'Paper') {
+      return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    } else {
+      return "This is a draw";
+    }
+  }
+
+  else if (playerSelection == 'Paper') {
+    if (computerSelection == 'Scissors') {
+      return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    } else if (computerSelection == 'Rock') {
+      return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+      return "This is a draw";
+    }
+  }
+
+  else if (playerSelection == 'Scissors') {
+    if (computerSelection == 'Rock') {
+      return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    } else if (computerSelection == 'Paper') {
+      return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+      return "This is a draw";
+    }
+  }
+}
